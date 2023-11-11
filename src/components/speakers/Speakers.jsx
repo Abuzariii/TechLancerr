d5e52dfec7801564f78d18a9b052ee3b2e4795e4
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-11-11 11:55:15 +0500

    Footer

---
+++ src/components/speakers/Speakers.jsx
@@ -0,0 +1,9 @@
+import classes from "./Speakers.module.css";
+
+export default function Speakers() {
+  return (
+    <div className={classes.speakers}>
+      <h1>Speakers</h1>
+    </div>
+  );
+}
