62ff1c94501449d1a7656132974237b4a3504464
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-11-11 11:52:47 +0500

    git push origin

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,21 @@
 import classes from "./Footer.module.css";
-import techalncer from "../../../public/Techlancer logo.png";
+import techlancerrSmall from "public/techlancerr-small.png";
 import { BsFacebook, BsInstagram, BsLinkedin, BsTiktok } from "react-icons/bs";
 import { FaXTwitter } from "react-icons/fa6";
 import Image from "next/image";
+import { poppins400, poppins } from "@/utils/fonts";
+import { IoPin } from "react-icons/io5";
 
 export default function Footer() {
   return (
     <div className={classes.footer}>
       <div className={classes.up}>
         <div>
-          <h3>Quick Links</h3>
-          <p>Contact Us</p>
-          <p>Register</p>
-          <p>Partners</p>
-          <p>Speakers</p>
+          <h3 className={poppins.className}>Quick Links</h3>
+          <p className={poppins400.className}>Contact Us</p>
+          <p className={poppins400.className}>Register</p>
+          <p className={poppins400.className}>Partners</p>
+          <p className={poppins400.className}>Speakers</p>
         </div>
 
         <div className={classes.iconsDiv}>
@@ -34,12 +36,21 @@ export default function Footer() {
           <a href="https://twitter.com/iubsps" target="_blank" rel="noreferrer">
             <FaXTwitter color="white" size="25px" />
           </a>
+          <a href="https://twitter.com/iubsps" target="_blank" rel="noreferrer">
+            <BsLinkedin color="white" size="25px" />
+          </a>
+          <a href="https://twitter.com/iubsps" target="_blank" rel="noreferrer">
+            <BsTiktok color="white" size="25px" />
+          </a>
         </div>
       </div>
 
       <div className={classes.down}>
-        <Image src={techalncer} height={100} />
-        <p>IUB Science Promotion Society © 2023</p>
+        <Image src={techlancerrSmall} height={60} />
+        <p className={poppins400.className}>
+          <IoPin color="white" size="20px" className={classes.pin} />
+          13th December, Main Auditorium, The Islamia University of Bahawalpur
+        </p>
       </div>
     </div>
   );
