081bbc99d10da2b82d5265302950f791cf7fc942
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-11-11 16:59:28 +0500

    Partners

--- src/components/banner/navbar/Navbar.jsx
+++ src/components/banner/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import techlancerrSmall from "public/techlancerr-small.png";
 import classes from "./Navbar.module.css";
 import Image from "next/image";
 import { poppinsLight } from "@/utils/fonts";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -10,10 +11,19 @@ export default function Navbar() {
         <Image src={techlancerrSmall} height={60}></Image>
 
         <div className={classes.pages}>
-          <p className={poppinsLight.className}>SPEAKERS</p>
-          <p className={poppinsLight.className}>SPONSORS</p>
-          <p className={poppinsLight.className}>SCHEDULE</p>
-          <p className={poppinsLight.className}>CONTACT US</p>
+          <Link href={"/speakers"} className="link">
+            <p className={poppinsLight.className}>SPEAKERS</p>
+          </Link>
+
+          <Link href={"/sponsors"} className="link">
+            <p className={poppinsLight.className}>SPONSORS</p>
+          </Link>
+          <Link href={"/schedule"} className="link">
+            <p className={poppinsLight.className}>SCHEDULE</p>
+          </Link>
+          <Link href={"/contact-us"} className="link">
+            <p className={poppinsLight.className}>CONTACT US</p>
+          </Link>
         </div>
 
         <button className={poppinsLight.className}>REGISTER</button>
--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -1,19 +1,38 @@
 import classes from "./Partners.module.css";
 import { poppins } from "@/utils/fonts";
 import Image from "next/image";
-import fps from "../../../public/techlancerr-small.png";
+import css from "../../../public/partners/css.png";
+import devhack from "../../../public/partners/Devhack.png";
+import dsa from "../../../public/partners/DSA-03.png";
+import eroz from "../../../public/partners/e Rozgar.png";
+import graphicsco from "../../../public/partners/Graphics Co 2nd Logo.png";
+import gdsc from "../../../public/partners/Gdsc.png";
+import sps from "../../../public/partners/IUB Social Media.png";
+import oneAPI from "../../../public/partners/logo-oneapi-rwd.png";
+import microsoft from "../../../public/partners/Microsoft.png";
+import nftp from "../../../public/partners/NFTP.png";
+import pitb from "../../../public/partners/PITB.png";
+import rp9 from "../../../public/partners/Regional Plane 9.png";
+import stufreelancer from "../../../public/partners/Student Freelancer.png";
 
 export default function Partners() {
   return (
     <div className={classes.partners}>
       <h1 className={poppins.className}>OUR PARTNERS</h1>
       <div className={classes.main}>
-        <Image src={fps} height={80} />
-        <Image src={fps} height={80} />
-        <Image src={fps} height={80} />
-        <Image src={fps} height={80} />
-        <Image src={fps} height={80} />
-        <Image src={fps} height={80} />
+        <Image src={pitb} height={120} />
+        <Image src={nftp} height={120} />
+        <Image src={eroz} height={120} />
+        <Image src={rp9} height={120} />
+        <Image src={stufreelancer} height={120} />
+        <Image src={gdsc} height={120} />
+        <Image src={microsoft} height={120} />
+        <Image src={css} height={120} />
+        <Image src={devhack} height={120} />
+        <Image src={graphicsco} height={120} />
+        <Image src={dsa} height={120} />
+        <Image src={oneAPI} height={120} />
+        <Image src={sps} height={120} />
       </div>
     </div>
   );
