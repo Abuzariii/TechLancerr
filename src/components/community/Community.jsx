2e2b956afd9307b40c29e9285ef6f4aba860e905
Muhammad Faeez Shabbir <faizytech1st@gmail.com>
2023-11-16 00:17:24 +0500

    Fixed Styles, added register and contactus sec

--- src/components/community/Community.jsx
+++ src/components/community/Community.jsx
@@ -6,16 +6,21 @@ import { poppins400 } from "@/utils/fonts";
 export default function Community() {
   return (
     <div className={classes.community}>
-      <div className={classes.left}>
+      <div className={classes.content}>
         <h1 className={poppins400.className}>
-          6,500 AFFILIATES, ADVERTISERS, ECOM SELLERS, NETWORKS, AND TECH
-          SUPPLIERS WILL GATHER IN LAS VEGAS AT AFFILIATE SUMMIT WEST 2024, THE
-          BIGGEST AFFILIATE MARKETING EVENT IN THE WORLD. ASW IS THE HOME OF
-          AFFILIATE MARKETING... IN FACT OF AFFILIATES PREFER ASW OVER ANY OTHER
-          EVENT*. SEE YOU THERE.
+          We are excited to introduce you to an exceptional opportunity to make
+          a meaningful impact on the future of our students and the workforce.
+          The Islamia University of Bahawalpur is organizing a flagship event,
+          TechLancerr Summit. This event is designed to bridge the gap between
+          education and industry, offering students valuable insights, career
+          opportunities, and a clear path towards their future careers. To
+          motivate our youth to shift from primitive methods of freelancing to
+          introduce tech in freelancing. We are inviting tech companies to boost
+          learning and employment opportunities for tech students in South
+          Punjab.
         </h1>
       </div>
-      <div className={classes.right}>
+      <div className={classes.imageWrapper}>
         <Image src={person} className={classes.img} />
       </div>
     </div>
