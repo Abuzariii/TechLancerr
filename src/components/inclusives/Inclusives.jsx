0b5c069ccc8036b2955242592513396f7cf610e3
Muhammad Faeez Shabbir <faizytech1st@gmail.com>
2023-12-03 19:41:56 +0500

    Updated Event Inclusives

--- src/components/inclusives/Inclusives.jsx
+++ src/components/inclusives/Inclusives.jsx
@@ -1,40 +1,35 @@
+"use client";
 import classes from "./Inclusives.module.css";
-import { poppins400 } from "@/utils/fonts";
-import { poppins } from "@/utils/fonts";
-import character from "../../../public/characterPhone.png";
-import Image from "next/image";
+import { MdDoubleArrow } from "react-icons/md";
 
-export default function Inclusives() {
+const Inclusives = () => {
+  const inclusivesList = [
+    "Opening Ceremony and Keynote Speech",
+    "Networking Session",
+    "Panel Discussions and Workshops",
+    "Exhibition Area for Freelancers",
+    "Networking and Socializing Activities",
+    "Closing Ceremony and Award Distribution",
+  ];
+
   return (
     <div className={classes.schedule}>
-      <h1 className={`${poppins.className} headingBg`}>EVENT INCLUSIVES</h1>
+      <h1 className={`headingBg`}>EVENT INCLUSIVES</h1>
 
       <div className={classes.main}>
-        <div className={classes.left}>
-          <Image src={character} className={classes.character} />
-        </div>
-
-        <div className={classes.right}>
-          <li className={poppins400.className}>
-            Registration and Networking Session
-          </li>
-          <li className={poppins400.className}>
-            Opening Ceremony and Keynote Speech
-          </li>
-          <li className={poppins400.className}>
-            Panel Discussions and Workshops
-          </li>
-          <li className={poppins400.className}>
-            Exhibition Area for Freelancers
-          </li>
-          <li className={poppins400.className}>
-            Networking and Socializing Activities
-          </li>
-          <li className={poppins400.className}>
-            Closing Ceremony and Award Distribution
-          </li>
-        </div>
+        <ul>
+          {inclusivesList.map((inclusive, index) => (
+            <li key={index}>
+              <div className={classes.icon}>
+                <MdDoubleArrow size={30} />
+              </div>
+              {inclusive}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
-}
+};
+
+export default Inclusives;
