f4c75a31e8d601b2a1ccaebdbe55b38473af6b7c
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-12-10 20:32:12 +0500

    Fuck this shit

--- src/utils/dimensions.js
+++ src/utils/dimensions.js
@@ -1,30 +1,30 @@
-"use client";
+// "use client";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-function getWindowDimensions() {
-  if (typeof window !== "undefined") {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height,
-    };
-  }
-}
+// function getWindowDimensions() {
+//   if (typeof window !== "undefined") {
+//     const { innerWidth: width, innerHeight: height } = window;
+//     return {
+//       width,
+//       height,
+//     };
+//   }
+// }
 
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+// export default function useWindowDimensions() {
+//   const [windowDimensions, setWindowDimensions] = useState(
+//     getWindowDimensions()
+//   );
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+//   useEffect(() => {
+//     function handleResize() {
+//       setWindowDimensions(getWindowDimensions());
+//     }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []);
 
-  return windowDimensions;
-}
+//   return windowDimensions;
+// }
