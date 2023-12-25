d38f3ddf133a5aec8fd4bef40f04a4cd4326edfa
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-12-25 22:05:51 +0500

    Added new sponsors

--- src/utils/partners.js
+++ src/utils/partners.js
@@ -13,15 +13,15 @@ const partners = [
   },
   {
     id: 3,
-    name: "e Rozgar",
-    category: "partner",
-    logo: "/partners/e Rozgar.png",
-  },
-  {
-    id: 4,
     name: "Google Developer Student Club - IUB",
     category: "partner",
     logo: "/partners/GDSC.png",
+  },
+  {
+    id: 4,
+    name: "e Rozgar",
+    category: "partner",
+    logo: "/partners/e Rozgar.png",
   },
   {
     id: 5,
@@ -60,64 +60,144 @@ const partners = [
     logo: "/partners/Regional Plane 9.png",
   },
   {
-    id: 10,
+    id: 11,
     name: "DevHack Club",
     category: "partner",
     logo: "/partners/Devhack.png",
   },
   {
-    id: 11,
+    id: 12,
     name: "IUB Social Media",
     category: "partner",
     logo: "/partners/IUB Social Media.png",
   },
   {
-    id: 12,
+    id: 13,
     name: "Microsoft LSA",
     category: "partner",
     logo: "/partners/Microsoft.png",
   },
   {
-    id: 13,
+    id: 14,
     name: "Graphisco Creative Studio",
     category: "partner",
     logo: "/partners/graphisco.png",
   },
   {
-    id: 14,
-    name: "Graphisco Creative Studio",
-    category: "sponsor",
-    logo: "/partners/graphisco.png",
-  },
-  {
     id: 15,
+    name: "GitHub",
+    category: "partner",
+    logo: "/partners/github.png",
+  },
+
+  // Sponsors
+  {
+    id: 16,
     name: "Ensemble",
     category: "sponsor",
     logo: "/partners/Ensemble.png",
   },
   {
-    id: 16,
-    name: "Oric",
-    category: "sponsor",
-    logo: "/partners/Oric.png",
-  },
-  {
     id: 17,
-    name: "AZ Marketer",
+    name: "BixiSoft",
     category: "sponsor",
-    logo: "/partners/AZMarketer.png",
+    logo: "/partners/bixi.png",
   },
   {
     id: 18,
+    name: "Corvit Bahawalpur",
+    category: "sponsor",
+    logo: "/partners/corvit.png",
+  },
+  {
+    id: 19,
+    name: "Lead Surafce",
+    category: "sponsor",
+    logo: "/partners/leadSurface.png",
+  },
+  {
+    id: 20,
     name: "Infinite IT Solution",
     category: "sponsor",
     logo: "/partners/infinite.png",
   },
   {
-    id: 19,
-    name: "GitHub",
-    category: "partner",
-    logo: "/partners/github.png",
+    id: 21,
+    name: "Oric",
+    category: "sponsor",
+    logo: "/partners/Oric.png",
+  },
+  {
+    id: 22,
+    name: "Pinomz Technologies",
+    category: "sponsor",
+    logo: "/partners/pinomz.png",
+  },
+  {
+    id: 23,
+    name: "Smart Earning Studio",
+    category: "sponsor",
+    logo: "/partners/smartEarning.png",
+  },
+  {
+    id: 24,
+    name: "DTOUB Limited",
+    category: "sponsor",
+    logo: "/partners/dtoub.png",
+  },
+  {
+    id: 25,
+    name: "Graphisco Creative Studio",
+    category: "sponsor",
+    logo: "/partners/graphisco.png",
+  },
+
+  {
+    id: 26,
+    name: "AZ Marketer",
+    category: "sponsor",
+    logo: "/partners/AZMarketer.png",
+  },
+
+  {
+    id: 27,
+    name: "Al Hadi Garden",
+    category: "sponsor",
+    logo: "/partners/alHadi.png",
+  },
+
+  {
+    id: 28,
+    name: "Concordia Colleges",
+    category: "sponsor",
+    logo: "/partners/concordia.png",
+  },
+
+  {
+    id: 29,
+    name: "QIW Host",
+    category: "sponsor",
+    logo: "/partners/host.png",
+  },
+
+  {
+    id: 30,
+    name: "Mudassir Builders & Developers",
+    category: "sponsor",
+    logo: "/partners/mudassir.png",
+  },
+  {
+    id: 31,
+    name: "PFA",
+    category: "sponsor",
+    logo: "/partners/PFA.png",
+  },
+
+  {
+    id: 32,
+    name: "QB Films and Photography",
+    category: "sponsor",
+    logo: "/partners/qb.png",
   },
 ];
 
